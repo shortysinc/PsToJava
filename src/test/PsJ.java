d8bac9e0dc152521d445dd9010902c730853271e
Jorge R. <jorge.shortys@gmail.com>
2017-10-22 15:33:31 +0200

    all

---
+++ src/test/PsJ.java
@@ -0,0 +1,36 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PsJ {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		//String command = "powershell.exe  your command";
+		  //Getting the version
+		  String command = "powershell.exe  $PSVersionTable.PSVersion";
+		  // Executing the command
+		  Process powerShellProcess = Runtime.getRuntime().exec(command);
+		  // Getting the results
+		  powerShellProcess.getOutputStream().close();
+		  String line;
+		  System.out.println("Standard Output:");
+		  BufferedReader stdout = new BufferedReader(new InputStreamReader(
+		    powerShellProcess.getInputStream()));
+		  while ((line = stdout.readLine()) != null) {
+		   System.out.println(line);
+		  }
+		  stdout.close();
+		  System.out.println("Standard Error:");
+		  BufferedReader stderr = new BufferedReader(new InputStreamReader(
+		    powerShellProcess.getErrorStream()));
+		  while ((line = stderr.readLine()) != null) {
+		   System.out.println(line);
+		  }
+		  stderr.close();
+		  System.out.println("Done");
+
+		 }
+	}
